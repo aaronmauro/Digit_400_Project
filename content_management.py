3d037b626b9d5b4793df3619931d2098bfa2695b
Aaron Mauro <mauro@psu.edu>
2018-03-20 19:44:56 +0000

    Initial Commit

---
+++ content_management.py
@@ -0,0 +1,12 @@
+def Content():
+    APP_CONTENT = {
+        "Home":[["Welcome to MyApp","/introduction-to-app/"],
+               ["What are the Issues","/background/"],
+               ["Connect with the Community","/messages/"],],
+        "Profile":[["User Profile","/profile/"],],
+        "Messages":[["Messages","/messages/"],],
+        "Settings":[["Terms of Service","/tos/"],
+                    ["Password Reset", "/passwordreset/"],
+                    ["Delete Account","/deleteaccount/"],]
+    }
+    return APP_CONTENT
---
+++ tests/calc.py
@@ -0,0 +1,18 @@
+class calculator:
+    def add(x, y):
+        added = x + y
+        print(added)
+        
+    def sub(x, y):
+        subbed = x - y
+        print(subbed)
+    
+    def multi(x, y):
+        multiply = x * y
+        print(multiply)
+        
+    def div(x, y):
+        divided = x / y
+        print(divided)
+        
+        
